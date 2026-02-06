3a795725848e09d0c791815c5872a9463e28a3b3
PETER CHEGE <emchegep@gmail.com>
2026-02-06 20:27:25 +0300

    Added post page and also fixed the Dockerfile.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -36,9 +36,30 @@ posts: list[dict] = [
 @app.get("/", include_in_schema=False, name="home")
 @app.get("/posts", include_in_schema=False, name="posts")
 def home(request: Request):
-    return templates.TemplateResponse(request, "home.html", {"posts":posts,
-                                                             "title": "Home"})
+    return templates.TemplateResponse(request, "home.html",
+                                      {"posts":posts,"title": "Home"})
+
+@app.get("/posts/{post_id}", include_in_schema=False, name="post")
+def post_page(request: Request,post_id: int):
+    for post in posts:
+        if post.get('id') == post_id:
+            title = post.get('title')[:50]
+            return templates.TemplateResponse(
+                request,
+                "post.html",
+                {"post": post, "title": title},
+            )
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found.")
+
 
 @app.get("/api/posts")
 def get_posts():
     return posts
+
+@app.get("/api/posts/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post.get('id') == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found.")
+
