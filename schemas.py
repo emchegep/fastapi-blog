72c4cc99032ba156ec34fdd780c2dffd9bb55201
PETER CHEGE <emchegep@gmail.com>
2026-02-11 18:21:03 +0300

    Refactored the exist post schemas to align with the User schema.

--- schemas.py
+++ schemas.py
@@ -24,14 +24,16 @@ class UserResponse(UserBase):
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
     content: str = Field(min_length=1)
-    author: str = Field(min_length=1, max_length=50)
+
 
 class PostCreate(PostBase):
-    pass
+    user_id: int # TEMPORARY
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
-    date_posted: str
+    user_id: int
+    date_posted: datetime
+    author: UserResponse
 
