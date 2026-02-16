f8aaa2c286c0a8b220b1c303aa00f16dcd68a4c5
PETER CHEGE <emchegep@gmail.com>
2026-02-16 11:44:13 +0300

    Implemented a patch api endpoint for partial post update.

--- main.py
+++ main.py
@@ -238,6 +238,50 @@ def update_post_full(
 
     return post
 
+@app.patch("/api/posts/{post_id}", response_model=PostResponse)
+def update_post_partial(
+        post_id: int,
+        post_data: PostUpdate,
+        db: Annotated[ Session, Depends(get_db)],
+):
+    result = db.execute(
+        select(models.Post).where(models.Post.id == post_id)
+    )
+    post = result.scalars().first()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found."
+        )
+
+    update_data = post_data.model_dump(exclude_unset=True)
+
+    for field, value in update_data.items():
+        setattr(post, field, value)
+
+    db.commit()
+    db.refresh(post)
+    return post
+
+
+@app.delete("/api/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(
+        select(models.Post).where(models.Post.id == post_id)
+    )
+    post = result.scalars().first()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found."
+        )
+
+    db.delete(post)
+    db.commit()
+
+
 
 ## StarletteHTTPException Handler
 @app.exception_handler(StarletteHTTPException)
