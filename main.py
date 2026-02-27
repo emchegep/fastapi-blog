646ea15ec9395cb5e6a4f610a4561c4116b743f9
PETER CHEGE <emchegep@gmail.com>
2026-02-27 13:18:54 +0300

    Implemented authorization logic on both UI and API routes.

--- main.py
+++ main.py
@@ -126,6 +126,15 @@ async def register_page(request: Request):
         {"title": "Register"},
     )
 
+@app.get("/account", include_in_schema=False)
+async def account_page(request: Request):
+    return templates.TemplateResponse(
+        request,
+        "account.html",
+        {"title": "Account"},
+    )
+
+
 
 ## StarletteHTTPException Handler
 @app.exception_handler(StarletteHTTPException)
--- schemas.py
+++ schemas.py
@@ -41,7 +41,7 @@ class PostBase(BaseModel):
 
 
 class PostCreate(PostBase):
-    user_id: int # TEMPORARY
+    pass
 
 class PostUpdate(BaseModel):
     title: str | None = Field(default=None, min_length=1, max_length=100)
